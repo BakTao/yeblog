1e7ba66b6244a65eb7f74ba6237e2d99ac49d3e6
叶百韬 <1363052695@qq.com>
2020-03-12 18:22:39 +0800

    3/12 用户查询、博客查询

--- src/main/java/com/tao/yeblog/model/dto/UserDTO.java
+++ src/main/java/com/tao/yeblog/model/dto/UserDTO.java
@@ -4,6 +4,7 @@ import lombok.Data;
 
 @Data
 public class UserDTO {
+
     private String name;    //名字
 
     private String loginId; //登录Id
@@ -17,4 +18,15 @@ public class UserDTO {
     private String lastLogIp;   //上次登录ip
 
     private String lastLogTime; //上次登录时间
+
+    private String userPhoto; //头像
+
+    private String regTime; //注册时间
+
+    private String enable; //是否有效
+
+    private String reason; //失效原因
+
+    private String blogCount; //博客数量
+
 }
